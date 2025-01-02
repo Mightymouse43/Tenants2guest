05726a0ad7bc2e68c10fb01b18cba787c53bdf21
Mightymouse43 <carlosdeportes92@gmail.com>
2025-01-02 15:09:26 -0800

    changed to airbnb page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -39,7 +39,7 @@ export default function Hero() {
               Compliance Protocol
             </Link>
             <a
-              href="#listings"
+              href="https://www.airbnb.com/users/show/278040839"
               className="inline-flex items-center justify-center px-6 py-3 bg-blue-600 text-white hover:bg-blue-700 transition rounded-lg"
             >
               <Home className="w-5 h-5 mr-2" />
